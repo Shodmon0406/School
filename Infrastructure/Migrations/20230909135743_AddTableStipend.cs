dc0133ae92936491b6e2d7c9e536a626a61e8925
Shodmon0406 <133884946+Shodmon0406@users.noreply.github.com>
2023-09-09 19:01:14 +0500

    UpdateStipend

---
+++ Infrastructure/Migrations/20230909135743_AddTableStipend.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTableStipend : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stipends",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StudenId = table.Column<string>(type: "text", nullable: false),
+                    StudentId = table.Column<string>(type: "text", nullable: true),
+                    StipendType = table.Column<int>(type: "integer", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdateAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stipends", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Stipends_AspNetUsers_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stipends_StudentId",
+                table: "Stipends",
+                column: "StudentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stipends");
+        }
+    }
+}
---
+++ WebApi/ExtentionsMethods/AddSerices/AddService.cs
@@ -0,0 +1,39 @@
+﻿using Infrastructure.Data;
+using Infrastructure.Services.Claas;
+using Infrastructure.Services.ClassroomService;
+using Infrastructure.Services.ClassService;
+using Infrastructure.Services.StudentServices;
+using Infrastructure.Services.SubjectService;
+using Infrastructure.Services.TeacherServices;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.ExtensionMethods.RegisterService;
+
+public static class RegisterService
+{
+    public static void AddRegisterService(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<DataContext>(configure =>
+            configure.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+
+        services.AddScoped<IClassService, ClassService>();
+        services.AddScoped<IStudentService, StudentService>();
+        services.AddScoped<ITeacherService, TeacherService>();
+        services.AddScoped<IClassroomService, ClassroomService>();
+        services.AddScoped<ISubjectService, SubjectService>();
+
+        services.AddIdentity<IdentityUser, IdentityRole>(config =>
+        {
+            config.Password.RequiredLength = 4;
+            config.Password.RequireDigit = false; // must have at least one digit
+            config.Password.RequireNonAlphanumeric = false; // must have at least one non-alphanumeric character
+            config.Password.RequireUppercase = false; // must have at least one uppercase character
+            config.Password.RequireLowercase = false;  // must have at least one lowercase character
+        })
+            //for registering usermanager and signinmanger
+            .AddEntityFrameworkStores<DataContext>()
+            .AddDefaultTokenProviders();
+    }
+}
