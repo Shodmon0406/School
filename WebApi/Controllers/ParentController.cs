767d2968c1391952e7be6d6f489babe0d28a6b90
Bahrulloif <bahrulloif@gmail.com>
2023-09-11 13:45:51 +0500

    parent_olimp

---
+++ WebApi/Controllers/ParentController.cs
@@ -0,0 +1,47 @@
+using Domain.Dtos;
+using Domain.Filters.ParentFilter;
+using Domain.Responses;
+using Infrastructure.Services.ParentService.cs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ParentController : ControllerBase
+{
+    private readonly IParentService _parentService;
+
+    public ParentController(IParentService parentService)
+    {
+        _parentService = parentService;
+    }
+    [HttpGet("GetAllParents")]
+    async public Task<Response<List<GetParentDto>>> GetAllParents(GetParentFilter filter)
+    {
+        return await _parentService.GetAllParents(filter);
+    }
+    [HttpGet("GetParentById")]
+    async public Task<Response<GetParentDto>> GetParentById(int id)
+    {
+        return await _parentService.GetParentById(id);
+    }
+
+[HttpPost("AddParent")]
+async public    Task<Response<GetParentDto>> AddParent(AddParentDto parent)
+{
+    return await _parentService.AddParent(parent);
+}
+
+[HttpPut("UpdateParent")]
+ async public   Task<Response<GetParentDto>> UpdateParent(AddParentDto parent)
+ {
+   return await _parentService.UpdateParent(parent);
+ }
+
+ [HttpDelete("DeleteParent")]
+   async public Task<Response<bool>> DeleteParent(int id)
+   {
+    return await _parentService.DeleteParent(id);
+   }
+}
